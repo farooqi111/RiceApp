4b02bdddba89429dc3167069ecb1f17592999078
Muhammad Abdur Rehman Farooqi <38170390+farooqi111@users.noreply.github.com>
2019-07-24 20:06:37 +1000

    version 1.05

--- Model/MongoConnection.js
+++ Model/MongoConnection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb://farooqi:Assasin@123@ds163510.mlab.com:63510/heroku_2hgcdhms",
-  {
-    useNewUrlParser: true
-  }
-);
+mongoose
+  .connect(
+    "mongodb://farooqi:farooqi123@ds163510.mlab.com:63510/heroku_2hgcdhms",
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("mongo connected"))
+  .catch(err => console.log(err));
 const connection = mongoose.connection;
 connection.once("open", function() {
   console.log("Mongoose database connection is established successfully");
