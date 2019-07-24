ffd14fb71956f7b111478714d0df3bf8a2fdeaba
Muhammad Abdur Rehman Farooqi <38170390+farooqi111@users.noreply.github.com>
2019-07-25 09:37:40 +1000

    version 1.08

--- client/src/components/Api/API.js
+++ client/src/components/Api/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-/* export default axios.create({
-  baseURL: "http://localhost:4000"
+ export default axios.create({
+  baseURL: "https://farooqitraders.herokuapp.com"
 });
- */
+ 
 export default axios;
