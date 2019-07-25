9a2fbc216d56c68f1fce723cbdcc629aa6394e05
Muhammad Abdur Rehman Farooqi <38170390+farooqi111@users.noreply.github.com>
2019-07-25 13:47:16 +1000

    version 1.13

--- routes/ElectricityBills.js
+++ routes/ElectricityBills.js
@@ -9,7 +9,7 @@ router.route("/").get(function(req, res) {
       if (err) {
         console.log(err);
       } else {
-        res.json(bills);
+        res.send(bills);
       }
     })
     .sort({ _id: -1 });
